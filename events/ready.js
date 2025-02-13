1c077546279baa9460cf4fdf8fcee50a1541f30a
Sn4red <joaquin.arriaga08@gmail.com>
2025-02-13 12:40:58 -0500

    Code was added to ready.js to update the number of users in the Discord server, displayed in a locked voice channel.

--- events/ready.js
+++ events/ready.js
@@ -10,5 +10,14 @@ module.exports = {
         console.log('(.._.\\./._\\.(.\\/.)/.)(.\\/.___)');
         console.log('.)._.(/....\\.)../.).\\/.(\\___.\\');
         console.log('(____/\\_/\\_/(__/..\\____/(____/\n\n');
+
+        const guild = client.guilds.cache.get('1162912802701316146');
+        const channel = guild.channels.cache.get('1339465441327714397');
+
+        setInterval(() => {
+            const memberCount = guild.memberCount;
+
+            channel.setName(`Member Count: ${memberCount.toLocaleString()}`);
+        }, 600000);
     },
 };
