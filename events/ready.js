892573d4ef1fe03db9143e1cfca995bf858daa89
Sn4red <joaquin.arriaga08@gmail.com>
2025-05-15 00:59:26 -0400

    Modify 'ready.js' and 'deploy-commands.js'
    'ready.js': the algorithm for counting the members was improved, by
    just counting the users that have the Member role,
    
    'deploy-commands.js': code on line 1 was deleted because the file
    'index-bayus.js' is already loading the environment variables.

--- events/ready.js
+++ events/ready.js
@@ -11,9 +11,9 @@ module.exports = {
         console.log('.)._.(/....\\.)../.).\\/.(\\___.\\');
         console.log('(____/\\_/\\_/(__/..\\____/(____/\n\n');
 
-        // * This obtains the SCP Collector ID, the Bot role ID and the Member Count channel ID.
+        // * This obtains The Bunk3r server and the Member role.
         const guild = client.guilds.cache.get(process.env.DISCORD_SERVER_ID);
-        const botRole = guild.roles.cache.get(process.env.DISCORD_BOT_ROLE_ID);
+        const memberRole = guild.roles.cache.get(process.env.DISCORD_MEMBER_ROLE_ID);
         const memberCountchannel = guild.channels.cache.get(process.env.DISCORD_MEMBER_COUNT_CHANNEL_ID);
 
         // * The function is executed every 10 minutes to get the total amount of users in the server,
@@ -22,13 +22,9 @@ module.exports = {
             // * This loads all members to the cache, so the Discord API can retrieve the most recent data.
             await guild.members.fetch();
 
-            let totalUserCount = 0;
-            const userCount = guild.memberCount;
-            const botCount = botRole.members.size;
+            const memberCount = memberRole.members.size;
 
-            totalUserCount = userCount - botCount;
-
-            memberCountchannel.setName(`Member Count: ${totalUserCount.toLocaleString()}`);
+            memberCountchannel.setName(`Member Count: ${memberCount.toLocaleString()}`);
         }, 600000);
     },
 };
