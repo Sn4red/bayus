2b5ad900103cc94a46b8695f3f6ba5eef1a6ca85
Sn4red <joaquin.arriaga08@gmail.com>
2025-06-24 16:34:06 -0400

    Move firestore query inside setInterval function
    The firestore query was being executed only once when the 'ClientReady'
    event was triggered. This change moves the query inside the setInterval
    function to ensure it runs every 10 minutes. It was also moved into a
    function to improve readability.

--- events/ready.js
+++ events/ready.js
@@ -16,16 +16,12 @@ module.exports = {
         console.log('.)._.(/....\\.)../.).\\/.(\\___.\\');
         console.log('(____/\\_/\\_/(__/..\\____/(____/\n\n');
 
-        // * This obtains the total amount of users in the database.
-        const userReference = database.collection('user');
-        const userSnapshot = await userReference.count().get();
-        const userCount = userSnapshot.data().count;
-
         // * This obtains The Bunk3r server and the Member role.
         const guild = client.guilds.cache.get(process.env.DISCORD_SERVER_ID);
         const memberRole = guild.roles.cache
             .get(process.env.DISCORD_MEMBER_ROLE_ID);
 
+        // * Obtains the channels.
         const memberCountChannel = guild.channels.cache
             .get(process.env.DISCORD_MEMBER_COUNT_CHANNEL_ID);
 
@@ -34,23 +30,38 @@ module.exports = {
 
         // * The function is executed every 10 minutes to get the total amount
         // * of users in the server with the Member role, and update the channel
-        // * name with the new amount.
+        // * name with the new amount, as well as the total amount of users
+        // * in the firestore database.
         setInterval(async () => {
             try {
                 // * This loads all members to the cache, so the Discord API can
-                // * retrieve the most recent data.
-                await guild.members.fetch();
+                // * retrieve the most recent data, and waits up to 30 seconds
+                // * to avoid timeouts.
+                await guild.members.fetch({ time: 30000 });
+
+                const memberCount = memberRole.members.size;
+                const userCount = await getUserCount();
+
+                console.log(memberCount);
+                console.log(userCount);
+
+                memberCountChannel
+                    .setName(`Member Count: ${memberCount.toLocaleString()}`);
+                scpUsersOnlineChannel
+                    .setName(`SCP Users Online: ${userCount.toLocaleString()}`);
             } catch (error) {
                 console.log(`${new Date()} >>> *** ERROR: ready.js ***`);
                 console.error(error);
             }
-
-            const memberCount = memberRole.members.size;
-
-            memberCountChannel
-                .setName(`Member Count: ${memberCount.toLocaleString()}`);
-            scpUsersOnlineChannel
-                .setName(`SCP Users Online: ${userCount.toLocaleString()}`);
         }, 600000);
     },
 };
+
+// * This function obtains the total amount of users in the database.
+async function getUserCount() {
+    const userReference = database.collection('user');
+    const userSnapshot = await userReference.count().get();
+    const userCount = userSnapshot.data().count;
+
+    return userCount;
+}
